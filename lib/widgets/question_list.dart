2b7d91fb5522ece2cc401c3c4e276fceb856278f
oblomovite <robert.sheynin@gmail.com>
2021-03-13 16:18:28 -0500

    question_list additions

--- lib/widgets/question_list.dart
+++ lib/widgets/question_list.dart
@@ -1,10 +1,3 @@
-import 'dart:convert';
-
-import 'package:f_date/api/json_models/questionaire.dart' as Questionaire;
-import 'package:f_date/api/json_models/questionaire.dart';
-import 'package:f_date/api/json_parsers/json_parser.dart';
-// import 'package:f_date/widgets/quiestionaire.dart';
-// import 'package:f_date/api/json_parsers/json_parser.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:firebase_database/firebase_database.dart';
@@ -17,40 +10,64 @@ widget will appear
 
 */
 
-class QuestionList extends StatelessWidget {
-  // Future<List<QuestionaireElement>> _getQuestionaires() async { /// 1
-  //   /// 1
-  //   final db = await FirebaseDatabase.instance
-  //       .reference()
-  //       .child('questionaires')
-  //       .once();
-  //   return db.value;
+class QuestionPrompt extends StatelessWidget {
+  final String prompt;
+  final List<dynamic> responses;
+  const QuestionPrompt({this.prompt, this.responses});
+
+  // QuestionPrompt.fromMap(Map<String, dynamic> data) {
+  //   this.responses = data['responses'];
   // }
 
   @override
   Widget build(BuildContext context) {
-    // return FutureBuilder<List<QuestionaireElement>>( /// 1
+    /* Make this fade-in? */
+    return Container(
+      child: Column(children: <Widget>[
+        Row(children: [
+          Text(
+            prompt,
+            textAlign: TextAlign.center,
+          ),
+        ]),
+        for (var i in responses) Text(
+                i.toString(),
+                textAlign: TextAlign.center,
+        )
+      ]),
+    );
+  }
+}
 
+class QuestionList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return FutureBuilder<DataSnapshot>(
       future:
-          // _getQuestionaires(), /// 1
           FirebaseDatabase.instance.reference().child('myers-briggs').once(),
       builder: (BuildContext context, AsyncSnapshot snapshot) {
         List<Widget> children;
 
         final data = snapshot.data.value;
         if (snapshot.hasData && data != null) {
-          // print("snapshot.data.value: ${data}");
-
           final result = data; //[0];
           print("result: ${result}");
           print("result type: ${result.runtimeType}");
-          print("result check :: first prompt :: ${result[0]["prompt"]}");
-          print("result check :: first response set :: ${result[0]["responses"]}");
+          print(
+              "result check :: first prompt :: ${result[0]["prompt"].runtimeType}");
+          print(
+              "result check :: first response set :: ${result[0]["responses"].runtimeType}");
 
+          // List<String> example_responses = result[0]["responses"];
           children = <Widget>[
-            // for (i in )
             Text('example text'),
+            // QuestionPrompt()
+            QuestionPrompt(
+              prompt: result[0]["prompt"],
+              responses: result[0]["responses"] //.cast<List<String>>()
+              ,
+            )
+            // getQuestions(result),
           ];
         } else {
           children = <Widget>[Text("no data found")];
